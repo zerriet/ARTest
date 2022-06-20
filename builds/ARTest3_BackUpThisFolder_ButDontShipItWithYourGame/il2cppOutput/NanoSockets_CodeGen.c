b58f8d9058d044adea746eb1a78e2f3ac818bfb1
zerriet <63580721+zerriet@users.noreply.github.com>
2022-06-21 03:50:46 +0800

    first draft
    Ar mix with fusion

---
+++ builds/ARTest3_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/NanoSockets_CodeGen.c
@@ -0,0 +1,222 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Boolean NanoSockets.Socket::get_IsCreated()
+extern void Socket_get_IsCreated_m808C27EDEF63277B4567C6E4AE8BE8AF1772A701 (void);
+// 0x00000002 System.Int64 NanoSockets.Socket::op_Implicit(NanoSockets.Socket)
+extern void Socket_op_Implicit_m7BC3EDC627F0972C760B8B1D054DE2FC58E62CC2 (void);
+// 0x00000003 NanoSockets.Socket NanoSockets.Socket::op_Implicit(System.Int64)
+extern void Socket_op_Implicit_m2AADAB5844E14B52F46AE1DC1A572D9B16EF5603 (void);
+// 0x00000004 System.Boolean NanoSockets.Address::Equals(NanoSockets.Address)
+extern void Address_Equals_m19F1A04FCFA04C181170EFE7B99B12A27FA2C6F6 (void);
+// 0x00000005 System.Boolean NanoSockets.Address::Equals(System.Object)
+extern void Address_Equals_mDDBFB7BB02D6A7AF30ECBC0637B7CE220B7AC282 (void);
+// 0x00000006 System.Int32 NanoSockets.Address::GetHashCode()
+extern void Address_GetHashCode_m40F507481CB45DC401163A00C2C7DA21348A4D5F (void);
+// 0x00000007 System.String NanoSockets.Address::ToString()
+extern void Address_ToString_m8A446F2E577DF79B979E0EFCEA892BB5D119909F (void);
+// 0x00000008 NanoSockets.Address NanoSockets.Address::LocalhostIPv4(System.UInt16)
+extern void Address_LocalhostIPv4_m42D8109E38D3857F8CE2D41D500DCF30C189E71A (void);
+// 0x00000009 NanoSockets.Address NanoSockets.Address::Any(System.UInt16)
+extern void Address_Any_m1C304E2587D6CC908DDBC3EE961835F9EDEF243F (void);
+// 0x0000000A NanoSockets.Address NanoSockets.Address::CreateFromIpPort(System.String,System.UInt16)
+extern void Address_CreateFromIpPort_m3A66A25F9678F057273ABB8E6633995A6F943C2D (void);
+// 0x0000000B NanoSockets.Status NanoSockets.UDP::Initialize()
+extern void UDP_Initialize_m974288531A2E98B24DA7ABEB9B9660BC5478DFDF (void);
+// 0x0000000C System.Void NanoSockets.UDP::Deinitialize()
+extern void UDP_Deinitialize_m70DEB50985B9EDAF5D57CAF3CCE0738FBEB30B5E (void);
+// 0x0000000D System.Int64 NanoSockets.UDP::Create(System.Int32,System.Int32)
+extern void UDP_Create_mB8E02749E2F906B084D65AEAA4237EAFFD3D5511 (void);
+// 0x0000000E System.Void NanoSockets.UDP::Destroy(System.Int64&)
+extern void UDP_Destroy_mE612ACA3FADC31249F47E023FC44463E7B9B31D9 (void);
+// 0x0000000F System.Int32 NanoSockets.UDP::Bind(System.Int64,System.IntPtr)
+extern void UDP_Bind_mEB8C72DFB288107FA1DBEB8BD09976A331F793FC (void);
+// 0x00000010 System.Int32 NanoSockets.UDP::Bind(System.Int64,NanoSockets.Address&)
+extern void UDP_Bind_m30068999BD8A3D8AC1C429C6BF513C21F8DBB071 (void);
+// 0x00000011 System.Int32 NanoSockets.UDP::Connect(System.Int64,NanoSockets.Address&)
+extern void UDP_Connect_m3380578F65AE9FB3E645FFF7B3879F87EA291878 (void);
+// 0x00000012 NanoSockets.Status NanoSockets.UDP::SetOption(System.Int64,System.Int32,System.Int32,System.Int32&,System.Int32)
+extern void UDP_SetOption_m4BE87376EB327518B1E3BC1575576B6F0AF0841D (void);
+// 0x00000013 NanoSockets.Status NanoSockets.UDP::GetOption(System.Int64,System.Int32,System.Int32,System.Int32&,System.Int32&)
+extern void UDP_GetOption_m148951ABBAC93AC835EC93110A3C74C0DD59245D (void);
+// 0x00000014 NanoSockets.Status NanoSockets.UDP::SetNonBlocking(System.Int64)
+extern void UDP_SetNonBlocking_m6DDBB60E445125A636F166D07C5B21CA0AE50AFA (void);
+// 0x00000015 NanoSockets.Status NanoSockets.UDP::SetDontFragment(System.Int64,System.Byte)
+extern void UDP_SetDontFragment_m3EED72F72837DCBED6C43DEDAA30F9D283F0F0A5 (void);
+// 0x00000016 System.Int32 NanoSockets.UDP::Send(System.Int64,System.IntPtr,System.IntPtr,System.Int32)
+extern void UDP_Send_m289BBD4981917CDF9E6B02B9D1C2B4C9D585B608 (void);
+// 0x00000017 System.Int32 NanoSockets.UDP::Send(System.Int64,System.IntPtr,System.Byte[],System.Int32)
+extern void UDP_Send_m905FC26FD4403AD07BA8330FFEE7E4EB271CFEF4 (void);
+// 0x00000018 System.Int32 NanoSockets.UDP::Send(System.Int64,NanoSockets.Address&,System.IntPtr,System.Int32)
+extern void UDP_Send_mB586FDFA2E916FC9595AD907F4FDCDDD25FE2904 (void);
+// 0x00000019 System.Int32 NanoSockets.UDP::Send(System.Int64,NanoSockets.Address&,System.Byte[],System.Int32)
+extern void UDP_Send_mE6B11135F63CC73795335539FD8186F6D2314597 (void);
+// 0x0000001A System.Int32 NanoSockets.UDP::Send(System.Int64,NanoSockets.Address*,System.Byte*,System.Int32)
+extern void UDP_Send_mB4E77C3253E93BA7E891736C5E44FA2A04F8ACDA (void);
+// 0x0000001B System.Int32 NanoSockets.UDP::Receive(System.Int64,System.IntPtr,System.IntPtr,System.Int32)
+extern void UDP_Receive_mA765F073EA132421534B9C291FE3398EF93F2501 (void);
+// 0x0000001C System.Int32 NanoSockets.UDP::Receive(System.Int64,System.IntPtr,System.Byte[],System.Int32)
+extern void UDP_Receive_mD89BAE2A19F999896D3E411FB60D0D4D06A5A31C (void);
+// 0x0000001D System.Int32 NanoSockets.UDP::Receive(System.Int64,NanoSockets.Address&,System.IntPtr,System.Int32)
+extern void UDP_Receive_m71C563F8A00D2420C5E2D256F2A63ADB1450944C (void);
+// 0x0000001E System.Int32 NanoSockets.UDP::Receive(System.Int64,NanoSockets.Address&,System.Byte[],System.Int32)
+extern void UDP_Receive_m03EE5E51DDDEBB465F94A61C40D14DACE8F3E186 (void);
+// 0x0000001F System.Int32 NanoSockets.UDP::Receive(System.Int64,NanoSockets.Address*,System.Byte*,System.Int32)
+extern void UDP_Receive_mF91410B929B04D7FC8F04ECD7F81B4F4E0BB1DD9 (void);
+// 0x00000020 System.Int32 NanoSockets.UDP::Poll(System.Int64,System.Int64)
+extern void UDP_Poll_m46165EEC3B4867BE25A27331E94699FF725819B2 (void);
+// 0x00000021 NanoSockets.Status NanoSockets.UDP::GetAddress(System.Int64,NanoSockets.Address&)
+extern void UDP_GetAddress_m68191BD29DB49C1C417B4D58C12A85D9A6B71B16 (void);
+// 0x00000022 NanoSockets.Status NanoSockets.UDP::IsEqual(NanoSockets.Address&,NanoSockets.Address&)
+extern void UDP_IsEqual_m778D89CEB76BC2D8A8A4B6E9032D2C161C94A04D (void);
+// 0x00000023 NanoSockets.Status NanoSockets.UDP::SetIP(NanoSockets.Address&,System.IntPtr)
+extern void UDP_SetIP_m3AF7DFFEE878C7F1B96C250FA1B4A53EF56FF551 (void);
+// 0x00000024 NanoSockets.Status NanoSockets.UDP::SetIP(NanoSockets.Address&,System.String)
+extern void UDP_SetIP_m02D59F214C84478AA5948FB1EC76567CA09D8DB8 (void);
+// 0x00000025 NanoSockets.Status NanoSockets.UDP::GetIP(NanoSockets.Address&,System.IntPtr,System.Int32)
+extern void UDP_GetIP_m0A5E0BCF613843E51A389A9F697B49B426D69A15 (void);
+// 0x00000026 NanoSockets.Status NanoSockets.UDP::GetIP(NanoSockets.Address&,System.Text.StringBuilder,System.Int32)
+extern void UDP_GetIP_m2A26D92F2E0B3F23E9B25EC1226D8CA55B9F7B76 (void);
+// 0x00000027 NanoSockets.Status NanoSockets.UDP::SetHostName(NanoSockets.Address&,System.IntPtr)
+extern void UDP_SetHostName_mF0B28D975745F908A41F5F6B4710DF9F1F3DC18B (void);
+// 0x00000028 NanoSockets.Status NanoSockets.UDP::SetHostName(NanoSockets.Address&,System.String)
+extern void UDP_SetHostName_m7008CDC5B49AB295BFC0C5D4E3D3D8F8136FCA03 (void);
+// 0x00000029 NanoSockets.Status NanoSockets.UDP::GetHostName(NanoSockets.Address&,System.IntPtr,System.Int32)
+extern void UDP_GetHostName_m21473B177693DD8A48886DDCE95031C7559F61A4 (void);
+// 0x0000002A NanoSockets.Status NanoSockets.UDP::GetHostName(NanoSockets.Address&,System.Text.StringBuilder,System.Int32)
+extern void UDP_GetHostName_mAC26FDB832135AD9CBAB8B01056C2648C9784F9C (void);
+static Il2CppMethodPointer s_methodPointers[42] = 
+{
+	Socket_get_IsCreated_m808C27EDEF63277B4567C6E4AE8BE8AF1772A701,
+	Socket_op_Implicit_m7BC3EDC627F0972C760B8B1D054DE2FC58E62CC2,
+	Socket_op_Implicit_m2AADAB5844E14B52F46AE1DC1A572D9B16EF5603,
+	Address_Equals_m19F1A04FCFA04C181170EFE7B99B12A27FA2C6F6,
+	Address_Equals_mDDBFB7BB02D6A7AF30ECBC0637B7CE220B7AC282,
+	Address_GetHashCode_m40F507481CB45DC401163A00C2C7DA21348A4D5F,
+	Address_ToString_m8A446F2E577DF79B979E0EFCEA892BB5D119909F,
+	Address_LocalhostIPv4_m42D8109E38D3857F8CE2D41D500DCF30C189E71A,
+	Address_Any_m1C304E2587D6CC908DDBC3EE961835F9EDEF243F,
+	Address_CreateFromIpPort_m3A66A25F9678F057273ABB8E6633995A6F943C2D,
+	UDP_Initialize_m974288531A2E98B24DA7ABEB9B9660BC5478DFDF,
+	UDP_Deinitialize_m70DEB50985B9EDAF5D57CAF3CCE0738FBEB30B5E,
+	UDP_Create_mB8E02749E2F906B084D65AEAA4237EAFFD3D5511,
+	UDP_Destroy_mE612ACA3FADC31249F47E023FC44463E7B9B31D9,
+	UDP_Bind_mEB8C72DFB288107FA1DBEB8BD09976A331F793FC,
+	UDP_Bind_m30068999BD8A3D8AC1C429C6BF513C21F8DBB071,
+	UDP_Connect_m3380578F65AE9FB3E645FFF7B3879F87EA291878,
+	UDP_SetOption_m4BE87376EB327518B1E3BC1575576B6F0AF0841D,
+	UDP_GetOption_m148951ABBAC93AC835EC93110A3C74C0DD59245D,
+	UDP_SetNonBlocking_m6DDBB60E445125A636F166D07C5B21CA0AE50AFA,
+	UDP_SetDontFragment_m3EED72F72837DCBED6C43DEDAA30F9D283F0F0A5,
+	UDP_Send_m289BBD4981917CDF9E6B02B9D1C2B4C9D585B608,
+	UDP_Send_m905FC26FD4403AD07BA8330FFEE7E4EB271CFEF4,
+	UDP_Send_mB586FDFA2E916FC9595AD907F4FDCDDD25FE2904,
+	UDP_Send_mE6B11135F63CC73795335539FD8186F6D2314597,
+	UDP_Send_mB4E77C3253E93BA7E891736C5E44FA2A04F8ACDA,
+	UDP_Receive_mA765F073EA132421534B9C291FE3398EF93F2501,
+	UDP_Receive_mD89BAE2A19F999896D3E411FB60D0D4D06A5A31C,
+	UDP_Receive_m71C563F8A00D2420C5E2D256F2A63ADB1450944C,
+	UDP_Receive_m03EE5E51DDDEBB465F94A61C40D14DACE8F3E186,
+	UDP_Receive_mF91410B929B04D7FC8F04ECD7F81B4F4E0BB1DD9,
+	UDP_Poll_m46165EEC3B4867BE25A27331E94699FF725819B2,
+	UDP_GetAddress_m68191BD29DB49C1C417B4D58C12A85D9A6B71B16,
+	UDP_IsEqual_m778D89CEB76BC2D8A8A4B6E9032D2C161C94A04D,
+	UDP_SetIP_m3AF7DFFEE878C7F1B96C250FA1B4A53EF56FF551,
+	UDP_SetIP_m02D59F214C84478AA5948FB1EC76567CA09D8DB8,
+	UDP_GetIP_m0A5E0BCF613843E51A389A9F697B49B426D69A15,
+	UDP_GetIP_m2A26D92F2E0B3F23E9B25EC1226D8CA55B9F7B76,
+	UDP_SetHostName_mF0B28D975745F908A41F5F6B4710DF9F1F3DC18B,
+	UDP_SetHostName_m7008CDC5B49AB295BFC0C5D4E3D3D8F8136FCA03,
+	UDP_GetHostName_m21473B177693DD8A48886DDCE95031C7559F61A4,
+	UDP_GetHostName_mAC26FDB832135AD9CBAB8B01056C2648C9784F9C,
+};
+extern void Socket_get_IsCreated_m808C27EDEF63277B4567C6E4AE8BE8AF1772A701_AdjustorThunk (void);
+extern void Address_Equals_m19F1A04FCFA04C181170EFE7B99B12A27FA2C6F6_AdjustorThunk (void);
+extern void Address_Equals_mDDBFB7BB02D6A7AF30ECBC0637B7CE220B7AC282_AdjustorThunk (void);
+extern void Address_GetHashCode_m40F507481CB45DC401163A00C2C7DA21348A4D5F_AdjustorThunk (void);
+extern void Address_ToString_m8A446F2E577DF79B979E0EFCEA892BB5D119909F_AdjustorThunk (void);
+static Il2CppTokenAdjustorThunkPair s_adjustorThunks[5] = 
+{
+	{ 0x06000001, Socket_get_IsCreated_m808C27EDEF63277B4567C6E4AE8BE8AF1772A701_AdjustorThunk },
+	{ 0x06000004, Address_Equals_m19F1A04FCFA04C181170EFE7B99B12A27FA2C6F6_AdjustorThunk },
+	{ 0x06000005, Address_Equals_mDDBFB7BB02D6A7AF30ECBC0637B7CE220B7AC282_AdjustorThunk },
+	{ 0x06000006, Address_GetHashCode_m40F507481CB45DC401163A00C2C7DA21348A4D5F_AdjustorThunk },
+	{ 0x06000007, Address_ToString_m8A446F2E577DF79B979E0EFCEA892BB5D119909F_AdjustorThunk },
+};
+static const int32_t s_InvokerIndices[42] = 
+{
+	5607,
+	8561,
+	8761,
+	3251,
+	3371,
+	5654,
+	5699,
+	8401,
+	8401,
+	7532,
+	8987,
+	9041,
+	7866,
+	8857,
+	7842,
+	7839,
+	7839,
+	6206,
+	6205,
+	8536,
+	7840,
+	6583,
+	6584,
+	6581,
+	6582,
+	6580,
+	6583,
+	6584,
+	6581,
+	6582,
+	6580,
+	7841,
+	7839,
+	7824,
+	7826,
+	7827,
+	7048,
+	7049,
+	7826,
+	7827,
+	7048,
+	7049,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_NanoSockets_CodeGenModule;
+const Il2CppCodeGenModule g_NanoSockets_CodeGenModule = 
+{
+	"NanoSockets.dll",
+	42,
+	s_methodPointers,
+	5,
+	s_adjustorThunks,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
---
+++ builds/ARTest_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,49 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void ARController::Update()
+extern void ARController_Update_mF6FF933C2A17DE72AFD2353EAD7FCCD46E839127 (void);
+// 0x00000002 System.Void ARController::.ctor()
+extern void ARController__ctor_mBE98AC7E425756F841ABFC40B93AE409D1489D88 (void);
+static Il2CppMethodPointer s_methodPointers[2] = 
+{
+	ARController_Update_mF6FF933C2A17DE72AFD2353EAD7FCCD46E839127,
+	ARController__ctor_mBE98AC7E425756F841ABFC40B93AE409D1489D88,
+};
+static const int32_t s_InvokerIndices[2] = 
+{
+	2086,
+	2086,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	2,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
